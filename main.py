a23f0133efd52b2f770f9904bb9cc56f961229ff
kifun <kifun.huang@gmail.com>
2020-12-21 16:42:44 +0800

    remove demofile

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ from utils.reader import TxtReader,ExcelReader
 from utils.writer import TxtWriter,ExcelWriter
 from utils.util import is_valid, get_reader, get_writer
 
-READER_CLASS = {
-    ".txt" : TxtReader.__name__,
-    ".xlsx" : ExcelReader.__name__
-}
-
-WRITER_CLASS = {
-    ".txt" : TxtWriter.__name__,
-    ".xlsx" : ExcelWriter.__name__
-}
-
-
 if __name__ == '__main__':
     filename = input("Please enter your filename: ")
     is_valid(filename)
